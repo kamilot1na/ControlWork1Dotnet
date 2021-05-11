656c70d3468554e277a85ce331e4f97805128c78
Kamil Nazmiev <kamilotina2001@gmail.com>
2021-05-11 20:18:47 +0300

    Bad code

---
+++ ControlWork1/Bird.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.Common;
+
+namespace ControlWork1
+{
+    public class Bird
+    {
+        public Bird()
+        {
+            
+        }
+        public int Id { get; set; }
+        public int Speed { get; set; }
+        
+        public bool isNailed { get; set; }
+        private int getBaseSpeed()
+        {
+            return Speed;
+        }
+
+        public Bird(int _Id, int _Speed, bool _isNailed)
+        {
+            Id = _Id;
+            Speed = _Speed;
+            isNailed = _isNailed;
+        }
+        
+        public double getSpeed(int Id) {
+            switch (Id) {
+                case 1:
+                    return getBaseSpeed();
+                case 2:
+                    return getBaseSpeed() - 5;
+                case 3:
+                    return (isNailed) ? 0 : getBaseSpeed();
+            }
+            throw new NotImplementedException("Should be unreachable");
+        }
+    }
+}
--- ControlWork1/Program.cs
+++ ControlWork1/Program.cs
@@ -6,7 +6,22 @@ namespace ControlWork1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Bird bird1 = new Bird();
+            bird1.Id = 1;
+            bird1.isNailed = false;
+            bird1.Speed = 50;
+            Bird bird2 = new Bird();
+            bird2.Id = 2;
+            bird2.isNailed = true;
+            bird2.Speed = 60;
+            Bird bird3 = new Bird();
+            bird3.Id = 3;
+            bird3.isNailed = false;
+            bird3.Speed = 70;
+
+            double birdSpeed = new Bird().getSpeed(1);
+            Console.WriteLine(birdSpeed);
+            Console.ReadLine();
         }
     }
 }
